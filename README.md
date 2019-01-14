ca1f5728538cc835b7d247994a098d81a5bdf40c
Noel Vock <noel1uk@gmail.com>
2019-01-14 21:30:17 +0000

    initial commit: functioning simple configuration server

---
+++ README.md
@@ -0,0 +1,8 @@
+# Config server
+
+A practice Springboot config server.
+
+## To run
+mvn spring-boot:run
+
+To get config data from exampleservice.yml visit http://localhost:8888/exampleservice/default
---
+++ src/main/java/com/intro/config/ConfigApplication.java
@@ -0,0 +1,17 @@
+package com.intro.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigApplication.class, args);
+	}
+
+}
+
